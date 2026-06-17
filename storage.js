f9e94c66db97104947bd1c6e90d6962b98ee2746
alcazarstore27-oss <alcazar.store27@gmail.com>
2026-06-17 13:57:00 -0600

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,52 @@
-// Storage module
+function getWords() {
+  return JSON.parse(
+    localStorage.getItem("words")
+  ) || [];
+}
+
+function saveWords(words) {
+  localStorage.setItem(
+    "words",
+    JSON.stringify(words)
+  );
+}
+
+function wordExists(word) {
+
+  const words = getWords();
+
+  const normalizedWord =
+    word.trim().toLowerCase();
+
+  return words.some(
+    w =>
+      w.word.toLowerCase() === normalizedWord
+  );
+}
+
+function addWordToStorage(word) {
+
+  const words = getWords();
+
+  words.push({
+    id: Date.now(),
+    word: word.trim().toLowerCase(),
+    level: "🟡 Aprendiendo",
+    correct: 0,
+    incorrect: 0,
+    mastery: 0
+  });
+
+  saveWords(words);
+}
+
+function deleteWordFromStorage(id) {
+
+  let words = getWords();
+
+  words = words.filter(
+    w => w.id !== id
+  );
+
+  saveWords(words);
+}
